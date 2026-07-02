109f651c435ec42f1c8d79fea7eb0347fc615e34
Areesha Soomro <areeshasoomro031@gmail.com>
2026-07-02 08:25:44 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { QuoteFlowApp } from './components/QuoteFlowApp';
+import './index.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <QuoteFlowApp />
+    </>
+  );
+};
+
+export default App;
